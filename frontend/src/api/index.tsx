d0839129e0f4559f5be1e9d0c2dac446973382ee
Racz Balint <raczbalint@Raczs-MacBook-Pro.local>
2022-09-19 11:45:11 +0200

    change axios baseUrl

--- frontend/src/api/index.tsx
+++ frontend/src/api/index.tsx
@@ -2,7 +2,7 @@ import { PinProps } from "App";
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mern-travel-app-backend.herokuapp.com",
+  baseURL: "https://mern-travel-app-backend.herokuapp.com/api",
 });
 
 export const axiosGetPins = () => API.get<PinProps[]>(`/pins`);
