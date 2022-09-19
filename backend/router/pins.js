22a480f57c2c2535f3937e2b569aa70971b487d5
Racz Balint <raczbalint@Raczs-MacBook-Pro.local>
2022-09-19 11:30:48 +0200

    change axios baseUrl

--- backend/router/pins.js
+++ backend/router/pins.js
@@ -15,16 +15,13 @@ router.post('/', async (req, res) => {
 
 // get all pins
 
-router.get(
-  'https://mern-travel-app-backend.herokuapp.com/api/pins',
-  async (req, res) => {
-    try {
-      const pins = await Pin.find()
-      res.status(200).json(pins)
-    } catch (err) {
-      res.status(500).json(err)
-    }
-  },
-)
+router.get('/', async (req, res) => {
+  try {
+    const pins = await Pin.find()
+    res.status(200).json(pins)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 
 module.exports = router
